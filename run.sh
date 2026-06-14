68526798f56ab03045f3ed923c27db505f8e6897
notatocha <veronika.shikina@icloud.com>
2026-06-14 22:46:12 +0000

    В run.sh добавлена реализация команды clear_data

--- run.sh
+++ run.sh
@@ -5,10 +5,13 @@ if [ "$1" = "build_generator" ]; then
 
 elif [ "$1" = "run_generator" ]; then
     docker run --rm -v "$(pwd)/data:/data" art-generator
-    
+
 elif [ "$1" = "build_reporter" ]; then
     docker build -t art-reporter ./reporter
 
 elif [ "$1" = "run_reporter" ]; then
     docker run --rm -v "$(pwd)/data:/app/data" art-reporter
+    
+elif [ "$1" = "clear_data" ]; then
+    rm -f data/*.csv data/*.html
 fi
